98decbcc2b01846d69a66dc9dc321dd31edbe346
Nathan Brewer <brewer.nathant@gmail.com>
2020-01-02 17:10:05 -0600

    update to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.txt", "r") as fh:
 
 setuptools.setup(
     name='Pyspectr',
-    version='0.2.1',
+    version='0.2.2',
     author='Nathan Brewer',
     author_email='brewer.nathant@gmail.com',
     packages=setuptools.find_packages(),
@@ -15,11 +15,12 @@ setuptools.setup(
     license='LICENSE.txt',
     description='Useful spectroscopic tools',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=(
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Scientific/Engineering :: Physics",
     ),
     requires=['matplotlib', 'numpy', 'lmfit'],
